cd25374a2e8fc9285bae080b674a1b4a905ad1cc
anton.serhiienko <anton.serhiienko@cd.p2h.com>
2023-12-18 18:47:44 +0200

    games init

--- src/pages/Games/Games.jsx
+++ src/pages/Games/Games.jsx
@@ -1,11 +1,20 @@
 import header from '../../assets/main/games-page/header.svg'
 import style from "./Games.module.scss"
+import AvailableGames from '../../components/home/available-games/AvailableGames'
+import CardsTitle from '../../components/games/CardsTitle'
+import LatestActivities from '../../components/home/latest-activities/LatestActivities'
+import LevelUp from '../../components/home/level-up/LevelUp'
+import Footer from '../../components/footer/Footer'
+import Retweet from '../../components/footer/retweet-and-get/Retweet'
 
 const Games = () => {
   return (
-    <div>
+    <div className={style.wrapper}>
       <img src={header} alt="" className={style.image} />
-      
+      <AvailableGames><CardsTitle /></AvailableGames>
+      <LatestActivities />
+      <LevelUp />
+      <Footer children={<Retweet />} />
     </div>
   )
 }
