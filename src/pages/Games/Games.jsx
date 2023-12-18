abd76b1201e131c71b805d94c28e7f039f725569
anton.serhiienko <anton.serhiienko@cd.p2h.com>
2023-12-18 18:00:21 +0200

    update cards styles

---
+++ src/pages/Games/Games.jsx
@@ -0,0 +1,13 @@
+import header from '../../assets/main/games-page/header.svg'
+import style from "./Games.module.scss"
+
+const Games = () => {
+  return (
+    <div>
+      <img src={header} alt="" className={style.image} />
+      
+    </div>
+  )
+}
+
+export default Games
